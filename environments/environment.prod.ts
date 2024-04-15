0c33b65b2da2f229a5c525eeb45269caa835217f
Mohsin Zaheer <mohsinmaken3@gmail.com>
2024-04-15 20:37:03 +0500

    production url changed

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment ={
     production:true,
-    apiUrl:'https://lets-chat-backend-0930a233a6ee.herokuapp.com/',
+    apiUrl:'https://lets-chat-backend-0930a233a6ee.herokuapp.com/api/',
     host:'',
     serverUrl:'https://lets-chat-backend-0930a233a6ee.herokuapp.com/'
 }
